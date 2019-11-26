1dff00af36b393a73ec2f9db747668834773fec5
juliancastaeda <1277@holbertonschool.com>
2019-11-26 10:39:49 -0500

    update functions.c file with own function _strcat

--- functions.c
+++ functions.c
@@ -70,3 +70,31 @@ int _strncmp(char *s1, char *s2, int n)
 	}
 	return (m);
 }
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: value dest to concatenar
+ *@src: second value to concatenar
+ *
+ * Return: char concatenado
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0, k;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+	while (src[j] != 0)
+	{
+		j++;
+	}
+	for (k = 0; k < j; k++)
+	{
+		dest[i] = src[k];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
