6731e5b934e9329b464256e3640a4c238d80cf2a
juliancastaeda <1277@holbertonschool.com>
2019-11-26 10:33:52 -0500

    update functions.c file with own function _strcpy.

--- functions.c
+++ functions.c
@@ -16,3 +16,26 @@ int _strlen(char *s)
 	}
 	return (i);
 }
+
+/**
+ * _strcpy - copies the string pointed by src to dest
+ *@dest: first value to check
+ *@src: Second value to check
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+	char *i = src;
+	int j = 0;
+
+	while (*i != '\0')
+	{
+		dest[j] = src[j];
+		i++;
+		j++;
+	}
+	dest[j] = src[j];
+	return (dest);
+}
