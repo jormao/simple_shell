3e9f75346c6115b33993bbd4572dd3d3ad4dc9a6
jormao <jormao@gmail.com>
2019-11-26 16:15:34 +0000

    my own atoi to convert string in integer to evaluate the number of the exit

--- split_string.c
+++ split_string.c
@@ -105,3 +105,36 @@ void index_function(char *buffer, char **envp)
 		perror("./hsh");
 	free_function(array_words);
 }
+
+
+/**
+ * _atoi - convert string to int
+ *@s: string to convert
+ *
+ * Return: integer converted
+ */
+int _atoi(char *s)
+{
+	unsigned int result = 0;
+	int i = 0, j = 0, a = 0, negat = 1;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] == '-')
+			negat = negat * -1;
+		while ((s[j] >= '0') && (s[j] <= '9'))
+		{
+			result = (result * 10) + (s[j] - '0');
+			j++;
+			a = 1;
+		}
+		if (a == 1)
+			j = i;
+	}
+	result = result * negat;
+	return (result);
+}
