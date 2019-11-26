7157de831347af2737351ebe943c20bf8e09bb74
juliancastaeda <1277@holbertonschool.com>
2019-11-26 10:59:56 -0500

    update more_functions.c file with own function found_path

--- more_functions.c
+++ more_functions.c
@@ -11,3 +11,24 @@ void sig_handler (int sig)
 	if (sig == SIGINT)
 	write(STDOUT_FILENO, "\n($) ", 5);
 }
+
+/**
+ * found_path - function for found the PATH
+ * @envp: pointer with the enviroment
+ *
+ * Return: pointer to PATH
+ */
+
+char *found_path(char **envp)
+{
+	int i = 0;
+	char *env = NULL;
+
+	while (envp[i])
+	{
+		if (!_strncmp("PATH", envp[i], 4))
+			env = envp[i] + 5;
+		i++;
+	}
+	return (env);
+}
